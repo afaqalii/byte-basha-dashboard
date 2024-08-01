36d36362256093d14545c253243199bcf5a5f582
Afaq <afaqali4244@gmail.com>
2024-08-01 15:04:06 +0500

    DOne with next js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'www.techiexpert.com',
-            'cdn.dribbble.com',
-            'miro.medium.com',
-            'www.goodworklabs.com',
-            'images.pexels.com',
-            'th.bing.com',
-            'assets.website-files.com',
-            'img.launchberg.com',
-            'firebasestorage.googleapis.com'
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.techiexpert.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.dribbble.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'miro.medium.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.goodworklabs.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'th.bing.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'assets.website-files.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.launchberg.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            },
         ],
     },
 };

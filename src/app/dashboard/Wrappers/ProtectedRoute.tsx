a190a9bdd910021cb928fdf204732c2a497e6c58
Afaq <afaqali4244@gmail.com>
2024-08-01 13:09:30 +0500

    Done with loading login issue

--- src/app/dashboard/Wrappers/ProtectedRoute.tsx
+++ src/app/dashboard/Wrappers/ProtectedRoute.tsx
@@ -21,13 +21,22 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
         setLoading(false);
     }, [router]);
 
-    if (loading) {
+    if (!isAuthenticated) {
         return (
             <div className="flex items-center justify-center min-h-screen">
                 <Spinner />
             </div>
         );
     }
+
+    // if (!isAuthenticated) {
+    //     return (
+    //         <div className="flex flex-col gap-5 items-center justify-center min-h-screen">
+    //             <h1 className="text-2xl font-semibold">You are not authorized</h1>
+    //             <Button onClick={() => router.push('/login')}>Sign in</Button>
+    //         </div>
+    //     );
+    // }
 
     return <>{children}</>;
 };
